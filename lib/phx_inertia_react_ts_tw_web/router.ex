3c9bb2c2a56b7719f637c4d29c3e8d3f8c678c30
sid <accounts@eleanorcare.ai>
2025-07-08 16:59:26 +0530

    Add inertia-phoenix

--- lib/phx_inertia_react_ts_tw_web/router.ex
+++ lib/phx_inertia_react_ts_tw_web/router.ex
@@ -8,6 +8,7 @@ defmodule PhxInertiaReactTsTwWeb.Router do
     plug :put_root_layout, html: {PhxInertiaReactTsTwWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Inertia.Plug
   end
 
   pipeline :api do
