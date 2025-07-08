5285a428f0d2e5e03002834f4b71d587ad88d91a
sid <accounts@eleanorcare.ai>
2025-07-08 17:38:13 +0530

    Add @inertiajs/react and connect with Phoenix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createInertiaApp } from "@inertiajs/react";
+import { createRoot } from "react-dom/client";
+import "./App.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createInertiaApp({
+  resolve: async (name) => {
+    const page = await import(`./pages/${name}.tsx`);
+    return page;
+  },
+  setup({ el, App, props }) {
+    createRoot(el).render(
+      <StrictMode>
+        <App {...props} />
+      </StrictMode>,
+    );
+  },
+});
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { loadEnv, defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "../priv/static",
+    target: ["es2022"],
+    rollupOptions: {
+      input: "src/main.tsx",
+      output: {
+        assetFileNames: "assets/[name][extname]",
+        chunkFileNames: "assets/[name].[hash].js",
+        entryFileNames: "assets/[name].js",
+      },
+    },
+    commonjsOptions: {
+      exclude: [],
+      // include: []
+    },
+  },
+  define: {
+    __APP_ENV__: env.APP_ENV,
+  },
 })
