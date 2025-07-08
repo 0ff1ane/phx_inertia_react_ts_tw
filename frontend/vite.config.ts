91ab11ec4b95fe40e0ba012d601f91afd1cedbea
sid <accounts@eleanorcare.ai>
2025-07-08 17:55:52 +0530

    Fix missing loadEnv in vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,25 +2,29 @@ import { loadEnv, defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    outDir: "../priv/static",
-    target: ["es2022"],
-    rollupOptions: {
-      input: "src/main.tsx",
-      output: {
-        assetFileNames: "assets/[name][extname]",
-        chunkFileNames: "assets/[name].[hash].js",
-        entryFileNames: "assets/[name].js",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tailwindcss()],
+    build: {
+      outDir: "../priv/static",
+      target: ["es2022"],
+      rollupOptions: {
+        input: "src/main.tsx",
+        output: {
+          assetFileNames: "assets/[name][extname]",
+          chunkFileNames: "assets/[name].[hash].js",
+          entryFileNames: "assets/[name].js",
+        },
+      },
+      commonjsOptions: {
+        exclude: [],
+        // include: []
       },
     },
-    commonjsOptions: {
-      exclude: [],
-      // include: []
+    define: {
+      __APP_ENV__: env.APP_ENV,
     },
-  },
-  define: {
-    __APP_ENV__: env.APP_ENV,
-  },
+  }
 })
